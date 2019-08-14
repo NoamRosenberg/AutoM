ad17ad69a6af346043efaf9a75d7edd200ba3e39
AI Wizard <noamsrosenberg@gmail.com>
2019-08-14 10:21:23 +0800

    old dataset default option changed to cityscapes

--- train_autodeeplab.py
+++ train_autodeeplab.py
@@ -285,7 +285,7 @@ def main():
                         help='opt level for half percision training (default: 01)')
     parser.add_argument('--out-stride', type=int, default=16,
                         help='network output stride (default: 8)')
-    parser.add_argument('--dataset', type=str, default='kd',
+    parser.add_argument('--dataset', type=str, default='cityscapes',
                         choices=['pascal', 'coco', 'cityscapes', 'kd'],
                         help='dataset name (default: pascal)')
     parser.add_argument('--autodeeplab', type=str, default='search',
